64bab71357f18007e5eecda68ff7ea3afddd9220
alix.wang <wangjun2012al@hotmail.com>
2020-11-25 00:28:45 +0800

    add style deal

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,13 @@
 import resolve from '@rollup/plugin-node-resolve'
+import postcss from 'rollup-plugin-postcss'
 import babel from '@rollup/plugin-babel'
+import postcssModules from 'postcss-modules'
 import path from 'path'
 
 export default {
   input: path.resolve(__dirname, 'src/index.tsx'),
   output: {
-    file: 'lib/lib.js',
+    file: 'lib/index.js',
     format: 'esm'
   },
   plugins: [
@@ -13,6 +15,13 @@ export default {
     babel({
       babelHelpers: 'runtime',
       extensions: ['jsx', 'tsx', 'ts', 'js']
+    }),
+    postcss({
+      extract: 'index.css',
+      modules: true,
+      plugins: [
+        postcssModules()
+      ]
     })
   ],
   external: ['react']
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import React, { FunctionComponent } from 'react'
+import style from './style/index.scss'
 
 const IndexListView:FunctionComponent = () => {
-  return <></>
+  return <div className={style.wrapper}></div>
 }
 
 export default IndexListView
